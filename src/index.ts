00cb8a51ee52f2b54a0f950a24fb9916648ca440
Dan Beaven <dm.beaven@gmail.com>
2025-06-13 10:23:52 +0100

    fix: build script

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 import { parsers as typescriptParsers } from 'prettier/plugins/typescript'
-import { Parser } from 'prettier'
-
 import { preprocess } from './peprocess.js'
 
-export default {
-  parsers: {
-    typescript: {
-      ...typescriptParsers.typescript,
-      preprocess: (text) => preprocess(text),
-    } satisfies Parser,
-  },
-}
+export default { parsers: { typescript: { ...typescriptParsers.typescript, preprocess } } }
